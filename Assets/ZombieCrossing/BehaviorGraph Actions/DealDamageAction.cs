e7abbd5f9c364168c222110d20f55e7b4adbc799
Liam Tolbert <treblotmail@gmail.com>
2024-11-02 02:51:02 -0400

    Creating Zombie AI (not done yet)

---
+++ Assets/ZombieCrossing/BehaviorGraph Actions/DealDamageAction.cs
@@ -0,0 +1,28 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Deal Damage", story: "[Agent] deals damage to [Target]", category: "Action", id: "ca4ac2b402e3a90eed31d4a520627e7d")]
+public partial class DealDamageAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
